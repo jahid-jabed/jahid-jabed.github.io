e127c86bd29ff3b2edd63db21c1a93964fcf84b4
Md. Jahidul Islam <jahid.jabed@gmail.com>
2022-05-15 00:30:40 +0600

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,28 @@
+function dayNightToggle() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    var check = window.localStorage.getItem('light-mode');
+    if (check == "light") {
+        window.localStorage.clear();
+    } else {
+        window.localStorage.setItem('light-mode', 'light');
+    }
+
+}
+
+function checkDayNight() {
+    var check = window.localStorage.getItem('light-mode');
+    var element = document.body;
+    if (check == "light") {
+        element.classList.add("light-mode");
+    } else {
+        element.classList.remove("light-mode");
+    }
+}
+
+checkDayNight()
+
+
 ;(function($){
     "use strict"
 	
